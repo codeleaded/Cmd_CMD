4f842c9d9ac25ec15c6931f63001180c93927869
codeleaded <codeleaded@gmail.com>
2025-11-03 16:26:37 +0100

    update

--- src/Main.c
+++ src/Main.c
@@ -1,4 +1,10 @@
+#if defined __linux__
 #include "/home/codeleaded/System/Static/Library/Cmd.h"
+#elif defined _WINE
+#include "/home/codeleaded/System/Static/Library/Cmd.h"
+#elif defined _WIN32
+#include "F:/home/codeleaded/System/Static/Library/Cmd.h"
+#endif
 
 int main(int argc,const char *argv[]){
     Session s = Session_New();
